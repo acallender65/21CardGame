374bee964743a56eb95c0f4ad0c2548651796d1a
acallender65 <amaracallender655@gmail.com>
2025-01-12 20:10:10 +0000

    Update cardscript.js

--- cardscript.js
+++ cardscript.js
@@ -6,6 +6,7 @@ var mainresult = -1;
 var ends = [];
 //console.log("byeryfhjk")
 var rounds = 1;
+//var score;
 
 async function getresults(deck){
     //console.log("hiya")
@@ -17,77 +18,78 @@ async function getresults(deck){
     return result;
 }
 
-window.onload = function() {
-    //var deck = new Deck();
-    //deck.shuffle()
-    //deck.init();
+//export default window.onload =
+function bjmg (score) {
+    var deck = new Deck();
+    deck.shuffle()
+    deck.init();
     //console.log(startgame(deck, mainresult, ends), "hiya");
-   // ends = startgame(deck, mainresult, ends).then((ends)=>{console.log(ends);});
+    ends = startgame(deck, mainresult, ends).then((ends)=>{console.log(ends, ends[0], ends[1]); score = score + ends[0]; ends=[]; console.log(score);});
 
-    function removeelements(){
-        var element = document.querySelector(".computercards");
-        element.innerHTML = (` `)
-        var element = document.querySelector(".yourcards");
-        element.innerHTML = (` `)
-        var element = document.querySelector(".pack");
-        element.innerHTML = (` `)
-        var element = document.querySelector(".computersum");
-        element.innerText = ""
-        var element = document.querySelector(".result");
-        element.innerText = ""
+    //function removeelements(){
+       // var element = document.querySelector(".computercards");
+        //element.innerHTML = (` `)
+        //var element = document.querySelector(".yourcards");
+       // element.innerHTML = (` `)
+       // var element = document.querySelector(".pack");
+       // element.innerHTML = (` `)
+       // var element = document.querySelector(".computersum");
+       // element.innerText = ""
+       // var element = document.querySelector(".result");
+        //element.innerText = ""
         //console.log("round2");
         //var deck = new Deck();
         //deck.shuffle()
         //return ends = startgame(deck, mainresult, ends).then((ends)=>{console.log(ends);});
-    }
+    //}
 
-    function createlistenertonextround (){
-        return new Promise((resolve) => {
-            document.querySelector(".keepcardsbutton").addEventListener("click", function(){
-                resolve(ends);
-            })
-        })
-    }
+    //function createlistenertonextround (){
+       // return new Promise((resolve) => {
+        //    document.querySelector(".keepcardsbutton").addEventListener("click", function(){
+        //        resolve(ends);
+        //    })
+       // })
+   // }
 
-    async function round1(ends){
-        var deck = new Deck();
-        deck.shuffle()
-        deck.init();
-        ends = startgame(deck, mainresult, ends, rounds).then((ends)=>{console.log(ends);  if (ends[1] == r2){
-            console.log("round2", ends);
-            score = ends[0];
-            rounds = rounds + 1;
-            ends = [];
-            removeelements();
-            round2(ends);
-        }});
+    //async function round1(ends){
+       // var deck = new Deck();
+        //deck.shuffle()
+        //deck.init();
+        //ends = startgame(deck, mainresult, ends, rounds).then((ends)=>{console.log(ends);  if (ends[1] == r2){
+        //    console.log("round2", ends);
+        //    score = ends[0];
+        //    rounds = rounds + 1;
+        //    ends = [];
+        //    removeelements();
+        //    round2(ends);
+        //}});
         //ends = startgame(deck, mainresult, ends)
         //console.log(ends, "hiya");
  
-    }
+    //}
 
-    function round2(ends){
-        var deck = new Deck();
-        deck.shuffle()
-        ends = startgame(deck, mainresult, ends, rounds).then((ends)=>{console.log(ends);if (ends[1] == r3){
-            console.log("round3");
-            rounds = rounds + 1;
-            removeelements();
-            round3();
-        }});
-    }
+    //function round2(ends){
+       // var deck = new Deck();
+        //deck.shuffle()
+        //ends = startgame(deck, mainresult, ends, rounds).then((ends)=>{console.log(ends);if (ends[1] == r3){
+        //    console.log("round3");
+        //    rounds = rounds + 1;
+        //    removeelements();
+        //    round3();
+        //}});
+    //}
 
-    function round3(){
-        var deck = new Deck();
-        deck.shuffle()
-        ends = startgame(deck, mainresult, ends, rounds).then((ends)=>{console.log(ends);});
-    }
+    //function round3(){
+    //    var deck = new Deck();
+    //    deck.shuffle()
+    //    ends = startgame(deck, mainresult, ends, rounds).then((ends)=>{console.log(ends);});
+    //}
 
-    round1(ends);
+    //round1(ends);
 
-    var score = -1;
-    var r2 = true;
-    var r3 = true;
+    //var score = -1;
+    //var r2 = true;
+    //var r3 = true;
     //removeelements();
     //round2()
     
@@ -103,6 +105,8 @@ window.onload = function() {
     //console.log(ends, "hiya");
     //ends = []
 };
+
+window.onload = bjmg(0);
 
 
 //window.onload();
